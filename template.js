4d944f9d83a0634d537de0855cd7e4ddc3b85978
Krithik Rao <krdevmail@gmail.com>
2019-06-29 16:19:48 -0400

    ADDED Basic Resume HTML File & Example Template
    This commit adds a new html file that is properly styled to display a
    resume document and print it out correctly. It sets margins and all
    necessary styling for printing.
    
    In addition, there is a lit-html component based template defined in
    template.js which actually initializes the different pieces of the
    template.
    
    All of these items will be loaded off of a resume.js file which defines
    the json for the resume.

---
+++ template.js
@@ -0,0 +1,117 @@
+import {html} from 'https://unpkg.com/lit-html@1.1.0?module';
+const header = (personal) => {
+  return html`
+  <style>
+  h1 {
+    border-bottom: 0.6pt solid #3F3F3F;
+    margin-top: 0in;
+    margin-bottom: 0.05in;
+  }
+  .contact-options {
+    font-size: 12pt;
+    list-style-type: none;
+    display: flex;
+    justify-content:space-between;
+    padding: 0px;
+    margin: 0px;
+  }
+  .contact-options li {
+    display:inline;
+  }
+  </style>
+  <h1>${personal.name} </h1>
+  <ul class="contact-options">
+    ${[...intersperse(personal.contact.map((option) => html`<li>${option}</li>`), html`<li>•</li>`)]}
+  </ul>
+  
+  `;
+};
+
+const subsection = (info) => {
+  return html`
+  <style>
+    .notes {
+      list-style: none;
+      padding-left: 0.2in;
+      margin: 0.05in 0in;
+      color: #3F3F3F;
+    }
+    .notes li:before {
+      content:"•"; 
+      font-size:11pt;
+      padding: 3pt;
+    }
+    .notes li {
+      margin: 2pt 0;
+    }
+    .subsection-head {
+      display: flex;
+      justify-content: space-between;
+    }
+    .left-head {
+      font-style: italic;
+    }
+  </style>
+  <div class="subsection-head">
+    <span class="left-head">${info.title}</span>
+    <span class="right-head">${info.duration}</span>
+  </div>
+  <ul class="notes">
+    ${info.notes.map((note) => html`<li>${note}</li>`)}
+  </ul>
+  `
+}
+
+const section = (info) => {
+  return html`
+  <style>
+    h2 {
+      margin-bottom: 0.08in;
+    }
+    .section {
+      border-left: 1pt solid black;
+      padding-left: 0.1in;
+      margin: 0.2in 0in;
+    }
+  </style>
+  <div class="section">
+    <h2>${info.title}</h2>
+    ${info.subheadings.map((heading) => subsection(heading))}
+  </div>
+  `;
+};
+
+export const template = (resumeJson) => {
+  return html`
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Libre+Franklin:400,700&display=swap');
+    .resume {
+      font-family: 'Libre Franklin', sans-serif;
+      font-size: 11pt;
+    }
+    h2 {
+      font-size: 12pt;
+      font-weight: 700;
+    }
+    h1 {
+      font-size: 24pt;
+      font-weight: 700;
+    }
+  </style>
+  <div class="resume">
+    ${header(resumeJson.personal)}
+    ${resumeJson.blocks.map((block) => section(block))} 
+  </div>
+  `;
+};
+
+// generator function to intersperse elements into an array
+function *intersperse(a, delim) {
+  let first = true;
+  for (const x of a) {
+    if (!first) yield delim;
+    first = false;
+    yield x;
+  }
+}
+
