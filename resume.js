8d6cdba33354aa2de23a41c595e3d33228cde369
Aaron Neustedter <aneusted@purdue.edu>
2019-07-10 11:27:30 -0400

    Added personal resume details

---
+++ resume.js
@@ -0,0 +1,104 @@
+export const resume = {
+  personal: {
+    name: "Aaron Neustedter",
+    seperator: "",
+    contact: [
+      "262-278-1106",
+      "aneusted@purdue.edu",
+      "linkedin.com/in/aneustedter"
+    ],
+  },
+  blocks: [
+    {
+      title: "Summary",
+      subheadings: [
+        {
+          title: "Graduate student at Purdue University pursuing a Master's of Computer Science. Customer Application Engineering\
+                    Intern for Wabtec/GE Transportation seeking a permanent position in spring 2020.",
+          notes: []
+        }
+      ]
+    },
+  
+    {
+      title: "Education",
+      subheadings: [
+        {
+          title: "Purdue University",
+          duration: "Fall 2016 - Spring 2020",
+          notes: [
+            "Master's in Computer Science - In Progress",
+            "Bachelor's in Computer Science - Software Engineering Concentration, Spring 2019", 
+            "Serves on the Purdue Waterski Team Executive Board as Treasurer",
+            "Research during undergrad centered around refactoring and implementing best software engineering practicing in existing research project"
+          ]
+        },
+      ]
+    },
+  
+    {
+      title: "Experience",
+      subheadings: [
+        {
+          title: "GE Transportation/Wabtec - Customer Application Engineering Intern",
+          duration: "Melbourne FL — Summer 2019",
+          notes: [
+            "Simulated train runs to ensure product viability of specific tracks",
+            "Developed tools to assist in data processing of simulations",
+            "Lead project to artificially inject error in track database to determine effects of an erroneous length of track"
+          ]
+        },
+        
+        {
+          title: "CNH Industrial - Systems/Model Based Design Engineering Intern",
+          duration: "New Holland PA — Summer 2018",
+          notes: [
+            "Supported the CaseIH Flagship Combine team with the development of the model year 2019 combine.",
+            "Specialized in the modeling and programming of the combine's automatic cleaning fan function and verification of a \
+                        safety-critical communication protocol between the operator's control handle and the processing module.",
+            "Assisted in start of a combine cabin simulator to allow embedded code to be tested in a semi-virtual environment."
+          ]
+        },
+        
+        {
+          title: "Rockwell Automation - Firmware Test Engineering Intern",
+          duration: "Mequon WI — Summer 2015 - 2017",
+          notes: [
+            "Developed automated firmware tests designed to replace manual testing of Rockwell’s drives",
+            "Automated test runtime of 1-20mins replaced manual tests of 4-40 man-hours",
+            "Created and equipped an automated test cell to develop and run automated tests",
+          ]
+        }
+      ]
+    },
+    
+    {
+      title: "Skills",
+      subheadings: [
+        {
+          notes: [
+            "Proficient in C/C++, Python, Bash, and Java",
+            "Familiar with ARM/x86, Bash and Matlab/Simulink",
+            "Formally educated at Purdue and experienced working in Agile/Scrum development methodologies",
+            "Repurposed a pair of desktop computers for use as Linux servers for backup, data storage, media streaming, and software development"
+          ]
+        }
+      ]
+    },
+  
+    {
+      title: "Volunteering",
+      subheadings: [
+        {
+        notes: [
+          "Mentored West Lafayette FRC Team 1747 high school students",
+          "Eagle Scout service project centered on creating awareness campaign for organ donation that resulted in signing \
+                up over 120 people to become organ donors",
+          "Hunted invasive lionfish, performed queen conch surveys, and performed reef health surveys in Beliz",
+          "Taught English and built an educational hydroponic farming system for children in Costa Rica"
+        ]
+      }
+    ]
+  },
+]
+};
